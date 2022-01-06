36a501273ee525b2aab4097332015f44668ee94a
Doniws <doniwahsap@gmail.com>
2022-01-06 17:11:47 +0700

    PERAPIAN

--- ExploreUniverse.js
+++ ExploreUniverse.js
@@ -1,9 +1,121 @@
-// sudah di pindahkan ke rocket html 
-      // SLIDE FEATURE 
-//  disini untuk feature rocket payload
+// PRELOADER
+var loader = document.getElementById("preloader")
+
+window.addEventListener("load", function() {
+    loader.style.display = "none";
+})
+
+// RIGHT CLICK
+window.addEventListener("contextmenu", function(event) {
+    event.preventDefault();
+    var contextElement = document.getElementById("context-menu");
+    contextElement.style.top = event.offsetY + "px";
+    contextElement.style.left = event.offsetX + "px";
+    contextElement.classList.add("active");
+});
+window.addEventListener("click", function() {
+    document.getElementById("context-menu").classList.remove("active");
+});
+
+// CURSOR CUSTOM 
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+    }
+
+)
+document.addEventListener('click', () => {
+        cursor.classList.add("expand");
+
+        setTimeout(() => {
+                cursor.classList.remove("expand");
+            }
+
+            , 200)
+    }
+
+)
+
+// SLIDE AREA
+
+// SLIDE LANDING 
+var slideIndex = 1;
+showslides(slideIndex);
+
+function nextSlides(n) {
+    showslides(slideIndex += n);
+}
+
+function currentslide(n) {
+    showslides(slideIndex = n);
+}
+
+function showslides(n) {
+    var i;
+    var slides = document.getElementsByClassName("myslides");
+    var dots = document.getElementsByClassName("fot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active-slide", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active-slide";
+}
+
+// SLIDE ROCKET 
+
+//EIGINE
+var slideIndex = 1;
+showslides(slideIndex);
+
+function nextSlides(n) {
+    showslides(slideIndex += n);
+}
+
+function currentslide(n) {
+    showslides(slideIndex = n);
+}
+
+function showslides(n) {
+    var i;
+    var slides = document.getElementsByClassName("myslides");
+    var dots = document.getElementsByClassName("fot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active-slide-2", "");
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active-slide-2";
+}
+
+// SLIDE FEATURE 
 var slideIndex = 1;
 showSlides(slideIndex);
-
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -28,6 +140,7 @@ function showSlides(n) {
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
+
     }
 
     for (i = 0; i < dots.length; i++) {
@@ -36,105 +149,23 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active-slide";
 }
-// GSAP AREA 
-// gsap.from('.rotation-robot img', {
-//     opacity: -1,
-//     ease: 'slow',
-//     duration: 12,
-//     x: -600
-// }
 
-// );
-
-// gsap.to('.rotaion img', {
-//     opacity: 1,
-//     ease: 'slow',
-//     duration: 10
-// }
-
-// );
-
-gsap.from('.logo img', {
-    opacity: -1,
-    ease: 'expo',
-    duration: 5,
-    scale: -5,
-    y: -1000
-}
-
-);
-
-
-gsap.from('.down', {
-    repeat: -1,
-    yoyo: true,
-    opacity: -1,
-    ease: 'power',
-    y: 100
-});
-
-gsap.from('img.copyimage', {
-    repeat: -1,
-    yoyo: true,
-    delay: 0,
-    repeatDelay: 1 ,
-    ease: 'bounce',
-    y: -10,
-    x: 10
-});
-
-
-
-
-// GSAP SCROLL
-
-
-
-tl.from(".marine", {
-    y: 100,
-    opacity: 0,
-    duration: 4,
-    delay: 0,
-    ease: 'expo'
-}
-, "=2")       
-
-// MENU 
-var menuList = document.getElementById("menuList");
-menuList.style.maxHeight = "0px";
-
-function togglemenu() {
-    if (menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "130px";
-    } else {
-        menuList.style.maxHeight = "0px";
-    }
-}
-
-
-
-
-
-
-// SLIDE LANDING 
-//  disini untuk landing pendaratan
+// PARALLAX 3 
 var slideIndex = 1;
-showslides(slideIndex);
-
+Showslides(slideIndex);
 
 function nextSlides(n) {
-    showslides(slideIndex += n);
+    Showslides(slideIndex += n);
 }
 
-function currentslide(n) {
-    showslides(slideIndex = n);
+function rentslide(n) {
+    Showslides(slideIndex = n);
 }
 
-function showslides(n) {
+function Showslides(n) {
     var i;
-    var slides = document.getElementsByClassName("myslides");
-    var dots = document.getElementsByClassName("fot");
-
+    var slides = document.getElementsByClassName("MySlides");
+    var dots = document.getElementsByClassName("tot");
 
     if (n > slides.length) {
         slideIndex = 1
@@ -149,73 +180,8 @@ function showslides(n) {
     }
 
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active-slide-2", "");
+        dots[i].className = dots[i].className.replace(" active-slide-", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active-slide-2";
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active-slide-";
 }
-
-// sudah dpindah ken rocket html 
-// SCROLL ROCKET 
-(function() {
-    let rocketPieces = document.querySelectorAll('.rocket-body span')
-    let rocket = document.querySelector('.rocket')
-    let triggerStart = window.innerHeight / 5;
-    let rocketOffsetTop = rocket.offsetTop;
-    let thirdOffsetTop = rocketPieces[2].offsetTop;
-
-    document.addEventListener('scroll', (e) => {
-            if (window.scrollY > (rocketOffsetTop - triggerStart)) {
-                rocketPieces[0].classList.add('active');
-                rocketPieces[1].classList.add('active');
-            } else {
-                rocketPieces[0].classList.remove('active');
-                rocketPieces[1].classList.remove('active');
-            }
-
-            if (window.scrollY > (thirdOffsetTop - triggerStart)) {
-                rocketPieces[2].classList.add('active');
-            } else {
-                rocketPieces[2].classList.remove('active');
-            }
-        }
-
-    )
-}
-
-())
-
-// Ini sudah di pindahkan ke rocket html 
-// gsap.from('.rocket-body .rocket1',{duration:40, y:1000,ease:'expo'});
-// gsap.from('.rocket-body .rocket2',{duration:42, y:1000,ease:'power4'});
-// gsap.from('.rocket-body .rocket3',{duration:44, y:1000,ease:'rough'});
-gsap.from('.floating-graphics img', {
-    repeat: -1,
-    yoyo: true,
-    opacity: 0,
-    ease: 'elastic',
-    delay: 1,
-    y: 50
-}
-
-);
-
-gsap.from('.floating-graphics2 img', {
-    repeat: -1,
-    yoyo: true,
-    opacity: -1,
-    ease: "back.inOut(1.7)",
-    y: -200,
-    y: 100
-}
-
-);
-gsap.from('.engines img', {
-    repeat: -1,
-    yoyo: true,
-    opacity: 0,
-    ease: 'elastic',
-    delay: 1
-}
-
-);
